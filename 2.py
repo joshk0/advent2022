534fa081853c6a9d601ae8182aa963b41fb4ac4d
Joshua Kwan <joshk@triplehelix.org>
2022-12-01 00:13:53 -0500

    Didn't need `compare_second`

--- 2.py
+++ 2.py
@@ -13,12 +13,4 @@ for line in sys.stdin:
     else:
         curr += int(line.rstrip())
 
-def compare_second(t1, t2):
-    if t1[1] < t2[1]:
-        return -1
-    elif t1[1] == t2[1]:
-        return 0
-    else:
-        return 1
-
 print(sum([a[1] for a in sorted(enumerate(elves), key=operator.itemgetter(1))[-3:]]))
