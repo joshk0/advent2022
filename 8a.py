450e74d1ba718fe3a3c3417bac56d8bab0ecd50b
Joshua Kwan <joshk@triplehelix.org>
2022-12-09 22:56:31 -0500

    day 8 is done

--- 8a.py
+++ 8a.py
@@ -3,7 +3,32 @@
 import sys
 
 grid = [[int(x) for x in line.rstrip()] for line in sys.stdin]
+visibilities = {}
 
-for y in range(1, len(grid) - 1):
-    for x in range(1, len(grid[0]) - 1):
-        this_height = grid[y][x]
+def is_edge(y, x):
+    return y == 0 or y == len(grid) - 1 or x == 0 or x == len(grid[0]) - 1
+
+visible_trees = 0
+s = ''
+for y in range(len(grid)):
+    for x in range(len(grid[0])):
+        visible = False
+        height = grid[y][x]
+        if is_edge(y, x):
+            visible = True
+        elif max(row[x] for row in grid[:y]) < height:
+            visible = True
+        elif max(row[x] for row in grid[y+1:]) < height:
+            visible = True
+        elif max(grid[y][:x]) < height:
+            visible = True
+        elif max(grid[y][x+1:]) < height:
+            visible = True
+        if visible:
+            s += 'V'
+            visible_trees += 1
+        else:
+            s += 'X'
+    s += '\n'
+
+print(visible_trees)
---
+++ 8b.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import sys
+
+grid = [[int(x) for x in line.rstrip()] for line in sys.stdin]
+visibilities = {}
+
+def is_edge(y, x):
+    return y == 0 or y == len(grid) - 1 or x == 0 or x == len(grid[0]) - 1
+
+def viewing_distance(seq, max_val):
+    for i, item in enumerate(seq):
+        if item >= max_val:
+            return seq[:i+1] # include current tree
+    return seq
+
+max_score = 0
+for y in range(len(grid)):
+    for x in range(len(grid[0])):
+        # North viewing distance
+        height = grid[y][x]
+
+        north_trees = [row[x] for row in grid[:y]][::-1]
+        north_score = len(viewing_distance(north_trees, height))
+
+        south_trees = [row[x] for row in grid[y+1:]]
+        south_score = len(viewing_distance(south_trees, height))
+        
+        east_trees = grid[y][x+1:]
+        east_score = len(viewing_distance(east_trees, height))
+
+        west_trees = grid[y][:x][::-1]
+        west_score = len(viewing_distance(west_trees, height))
+
+        score = north_score * south_score * west_score * east_score
+
+        if score > max_score:
+            max_score = score
+
+print(max_score)
